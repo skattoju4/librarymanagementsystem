1fdf688f8f0494ec757c1c21590c36629fd7e900
Saeed Jamali <42341043+SaeedJamaly@users.noreply.github.com>
2023-07-28 18:01:03 -0400

    Update PublisherController.java

--- src/main/java/com/knf/dev/librarymanagementsystem/controller/PublisherController.java
+++ src/main/java/com/knf/dev/librarymanagementsystem/controller/PublisherController.java
@@ -1,47 +1,80 @@
 package com.knf.dev.librarymanagementsystem.controller;
 
-// Import statements...
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.knf.dev.librarymanagementsystem.entity.Publisher;
+import com.knf.dev.librarymanagementsystem.service.PublisherService;
 
 @Controller
 public class PublisherController {
-    final PublisherService publisherService;
-    private static final String REDIRECT_PUBLISHERS = "redirect:/publishers";
 
-    public PublisherController(PublisherService publisherService) {
-        this.publisherService = publisherService;
-    }
+	final PublisherService publisherService;
 
-    // Existing methods...
+	public PublisherController(PublisherService publisherService) {
+		this.publisherService = publisherService;
 
-    @PostMapping("/add-publisher")
-    public String createPublisher(Publisher publisher, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return "add-publisher";
-        }
-        publisherService.createPublisher(publisher);
-        model.addAttribute("publisher", publisherService.findAllPublishers());
-        return REDIRECT_PUBLISHERS;
-    }
+	}
 
-    // Existing methods...
+	@RequestMapping("/publishers")
+	public String findAllPublishers(Model model) {
 
-    @PostMapping("/update-publisher/{id}")
-    public String updatePublisher(@PathVariable("id") Long id, Publisher publisher, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            publisher.setId(id);
-            return "update-publishers";
-        }
-        publisherService.updatePublisher(publisher);
-        model.addAttribute("publisher", publisherService.findAllPublishers());
-        return REDIRECT_PUBLISHERS;
-    }
+		model.addAttribute("publishers", publisherService.findAllPublishers());
+		return "list-publishers";
+	}
 
-    // Existing methods...
+	@RequestMapping("/publisher/{id}")
+	public String findPublisherById(@PathVariable("id") Long id, Model model) {
 
-    @GetMapping("/remove-publisher/{id}")
-    public String deletePublisher(@PathVariable("id") Long id, Model model) {
-        publisherService.deletePublisher(id);
-        model.addAttribute("publisher", publisherService.findAllPublishers());
-        return REDIRECT_PUBLISHERS;
-    }
+		model.addAttribute("publisher", publisherService.findPublisherById(id));
+		return "list-publisher";
+	}
+
+	@GetMapping("/addPublisher")
+	public String showCreateForm(Publisher publisher) {
+		return "add-publisher";
+	}
+
+	@RequestMapping("/add-publisher")
+	public String createPublisher(Publisher publisher, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			return "add-publisher";
+		}
+
+		publisherService.createPublisher(publisher);
+		model.addAttribute("publisher", publisherService.findAllPublishers());
+		return "redirect:/publishers";
+	}
+
+	@GetMapping("/updatePublisher/{id}")
+	public String showUpdateForm(@PathVariable("id") Long id, Model model) {
+
+		model.addAttribute("publisher", publisherService.findPublisherById(id));
+		return "update-publisher";
+	}
+
+	@RequestMapping("/update-publisher/{id}")
+	public String updatePublisher(@PathVariable("id") Long id, Publisher publisher, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			publisher.setId(id);
+			return "update-publishers";
+		}
+
+		publisherService.updatePublisher(publisher);
+		model.addAttribute("publisher", publisherService.findAllPublishers());
+		return "redirect:/publishers";
+	}
+
+	@RequestMapping("/remove-publisher/{id}")
+	public String deletePublisher(@PathVariable("id") Long id, Model model) {
+		publisherService.deletePublisher(id);
+
+		model.addAttribute("publisher", publisherService.findAllPublishers());
+		return "redirect:/publishers";
+	}
+
 }
